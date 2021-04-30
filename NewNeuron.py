6fc14c6d149ca89bb046a39ed6d9ffd7b7a41b68
Mickle Kozlov <33799845+Borntowarn@users.noreply.github.com>
2021-04-30 06:01:44 +0300

    Initial commit

---
+++ NewNeuron.py
@@ -0,0 +1,95 @@
+import numpy as np
+from numpy.random import seed
+
+class neuron:
+    temp = 0.001
+    x = np.array([[]])
+    y = np.array([])
+    eta = 0.001
+    cost = []
+    iter = 0
+    errors_ = []
+    w = np.array([])
+    prov1 = 0.0
+    prov2 = 0.0
+    prov3 = 0.0
+    prov4 = 0.0
+    random_s = 0
+
+    def __init__(self, iter, data, answer, eta, random_s = None):
+        self.eta = eta
+        self.iter = iter
+        self.x = data[0: ,0 : 3 : 2]
+        self.y = answer
+        self.prov1 = self.x[:, 0].mean()
+        self.prov2 = self.x[:, 1].mean()
+        self.prov3 = self.x[:, 0].std()
+        self.prov4 = self.x[:, 1].std()
+        self.w = np.zeros(1 + self.x.shape[1])
+        self.x[:, 0] = (self.x[:,0] - self.prov1) / self.prov3
+        self.x[:, 1] = (self.x[:,1] - self.prov2) / self.prov4
+        'self.x[:, 2] = (self.x[:,2] - self.x[:, 2].mean()) / self.x[:, 2].std()'
+        self.random_s = seed(random_s)
+
+    def clear_out(self, x):
+        return (np.dot(x, self.w[1:]) + self.w[0])
+    
+    def activation(self, x):
+        return (self.clear_out(x))
+       
+    def learning(self):
+        for i in range(self.iter):
+            self.x, self.y = self.shuffle(self.x, self.y)
+            e = 0.0
+            for q in range(len(self.y)):
+                error = self.y[q] - self.activation(self.x[q, :])
+                self.w[1:] += self.eta * self.x[q].T.dot(error)
+                self.w[0] += self.eta * error
+                e += (error**2) * 0.5
+            self.errors_.append(e)
+            print (self.errors_[i])
+            print (self.proverka())
+        return self
+
+    def shuffle(self, x, y):
+        r = np.random.permutation(len(y))
+        return x[r], y[r]
+
+    def predict(self,x):
+        """x = np.array(x);
+        if x.ndim == 1:
+            x[0] = (x[0] - self.prov1) / self.prov3
+            x[1] = (x[1] - self.prov2) / self.prov4
+        else:
+            x[:, 0] = (x[:,0] - self.prov1) / self.prov3
+            x[:, 1] = (x[:,1] - self.prov2) / self.prov4"""
+        return (np.where(self.clear_out(x) >= 0, 1, -1))
+
+    def proverka(self):
+        return (np.where(self.predict(self.x) == self.y, 1, 0).sum())
+
+x1 = []
+y1 = []
+with open("C:\\Users\\kozlo\\source\\repos\\Neuron\\NewNeuron\\data.txt", "r") as f:
+    for a in f:
+        a = a.strip().split()
+        x1.append(a[0:4])
+        if a[4] == "setosa":
+            y1.append(1)
+        else:
+            y1.append(-1)
+
+n = neuron(20, np.array(x1, dtype = float), np.array(y1), 0.02)
+n.learning()
+
+
+a = [float(s) for s in input().split()]
+
+while(a[0] != 0):
+    if (n.predict(a[0:]) == 1) : print("setosa")
+    else : print ("versicolor")
+    a = [float(s) for s in input().split()]
+
+#sovmaof
+
+

e0963730d0e2d0af6da821dd0483adcb4505ce5d
Mickle Kozlov <33799845+Borntowarn@users.noreply.github.com>
2021-05-22 13:21:39 +0300

    Update NewNeuron.py
    Аналогично. Пробы парзеновского окна с разными ядрами, шириной окна и метриками минковского.
    Вывод: для инфинитного ядра можно выбирать довольно малые h

--- NewNeuron.py
+++ NewNeuron.py
@@ -6,6 +6,9 @@ from sklearn.metrics import accuracy_score
 from scipy.spatial.distance import cdist
 from sklearn.svm import SVC
 import matplotlib.pyplot as plt
+from mlxtend.plotting import plot_decision_regions
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
 
 def classifier(data, xtrain, ytrain, h, n_neighbours = 100):
 	ytrain = np.array(ytrain)
@@ -13,16 +16,17 @@ def classifier(data, xtrain, ytrain, h, n_neighbours = 100):
 	distance = cdist(data, xtrain, 'euclidean')
 	indexes = np.array([i.argsort() for i in distance[:]])
 	distance /= h # h - размер окна Парзена
-	weights_ker = np.exp(-2*distance**2) # инфинитное гауссово ядро
+	weights_ker = np.exp(-2*distance**2) # ядро
 	v = []
 	for i, x in enumerate(data):
-		s = [np.where(ytrain[indexes[i, 0:n_neighbours]] == j, weights_ker[i, indexes[i, 0:n_neighbours]], 0).sum() for j in np.unique(ytrain)] # считает количество соседей разных классов вблизи объекта
+		s = [np.where(ytrain[indexes[i, 0:n_neighbours]] == j, weights_ker[i, indexes[i, 0:n_neighbours]], 0).sum() for j in np.unique(ytrain)] # считает веса соседей разных классов вблизи объекта
 		v.append(s.index(max(s))) # выбирает класс, вес объектов которого больше воздействует на объект
-		a = 0
-		b = 0
-		c = 0
 	return (v)
-	
+
+
+def weight(dist, h= 0.01):
+	dist /= h
+	return (np.exp(-2*dist**2))
 
 
 x1 = []
@@ -37,6 +41,19 @@ with open("C:\\Users\\kozlo\\source\\repos\\VSCODE PYTHON\\Neuron\\NewNeuron\\da
 			y1.append(1)
 		else: y1.append(2)
 
-x_train, x_test, y_train, y_test = train_test_split(np.array(x1),np.array(y1), test_size=0.3, random_state=0)
+x_train, x_test, y_train, y_test = train_test_split(np.array(x1)[:,0:3:2],np.array(y1), test_size=0.3, random_state=0)
 
-print(np.where(classifier(x_test, x_train, y_train, h=1,  n_neighbours = 100) == y_test, 1, 0).sum())
+x_train, x_test = [np.array(i, dtype=float) for i in[x_train, x_test]]
+y_train, y_test = [np.array(i, dtype=int) for i in[y_train, y_test]]
+
+model = KNeighborsClassifier(n_neighbors = 105, weights=weight)
+model.fit(x_train, y_train)
+
+print(np.where(model.predict(x_test) == y_test, 1, 0).sum())
+print(np.where(classifier(x_test, x_train, y_train, h=1,  n_neighbours = 105) == y_test, 1, 0).sum())
+
+fig, axes = plt.subplots(1,2)
+
+plot_decision_regions(x_train, y_train, model, ax=axes[0])
+plot_decision_regions(x_test, y_test, model, ax=axes[1])
+plt.show()
